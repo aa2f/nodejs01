d86fd28c373d07cb9dc3cc7c79d3502f3d257c94
aa2f <aa2f@hotmail.com>
2024-08-06 15:18:21 +0300

    add update

--- controller/products.js
+++ controller/products.js
@@ -7,11 +7,20 @@ module.exports = {
             result: products.map(res => {
                 return {
                     id: res.id,
+                    name: res.name,
                     price: res.price,
                     desc: res.desc
                 }
             })
         });
+    },
+    getProduct: async (req, res, next) => {
+        const id = req.params.id;
+        const product = await PRODUCTS.findById(id);
+
+
+
+        res.status(200).json(product);
     },
     createProduct: async (req, res) => {
         const product = await new PRODUCTS({
@@ -21,6 +30,36 @@ module.exports = {
         }).save();
 
         res.json({ "message": "Created successfully", "id": product.id, "name": product.name });
+    },
+
+    updateProduct: async (req,res) => {
+        const id = req.params.id;
+        const product = await PRODUCTS.findById(id);
+        if(product != null){
+            if(req.body.name !== null){
+                product.name = req.body.name 
+            }
+
+            if(req.body.price !== null){
+                product.price = req.body.price 
+            }
+
+            if(req.body.desc !== null){
+                product.desc = req.body.desc 
+            }
+
+            try {
+                const updatedProduct = await product.save() //saving in DB
+                res.send(updatedProduct) // sending back as response
+              } catch (err) {
+                res.status(400).json({ message: err.message })
+              }
+
+
+        }else{
+            res.json({"message":"No data found"})
+        }
+        
     },
 
     detelteProduct: async (req,res) => {
--- route/product.js
+++ route/product.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getProducts, createProduct, detelteProduct} = require('../controller/products');
+const {getProducts, createProduct, detelteProduct, getProduct, updateProduct} = require('../controller/products');
 
 router.get('/',getProducts);
+router.get('/:id',getProduct);
 router.post('/',createProduct);
+router.patch('/:id',updateProduct)
 router.delete('/:id',detelteProduct)
 
 module.exports = router
